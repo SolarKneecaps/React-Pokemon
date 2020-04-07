c871ba5c4351f8c4411397bc9b9436f590677924
Scott Plomer <scplomer27@gmail.com>
2020-04-07 00:21:53 -0500

    Updates

---
+++ precache-manifest.1a0785b9c5bc289a5e35c32bc648cdbb.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fd3b90be31e9bc3c7fac08557d8e52a7",
+    "url": "/React-Pokemon/index.html"
+  },
+  {
+    "revision": "6a28332a481b66c70f05",
+    "url": "/React-Pokemon/static/css/main.e79dcdfb.chunk.css"
+  },
+  {
+    "revision": "dab3f6a81d1dba2a367f",
+    "url": "/React-Pokemon/static/js/2.f72e8db5.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/React-Pokemon/static/js/2.f72e8db5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6a28332a481b66c70f05",
+    "url": "/React-Pokemon/static/js/main.c319a107.chunk.js"
+  },
+  {
+    "revision": "48a61b99b560003c2147",
+    "url": "/React-Pokemon/static/js/runtime-main.1d01fd99.js"
+  },
+  {
+    "revision": "07204c807891600829b5f6985c40acd6",
+    "url": "/React-Pokemon/static/media/gamebackgroundOneFighting.07204c80.jpg"
+  }
+]);
